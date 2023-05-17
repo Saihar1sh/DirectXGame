a1d646b7d39c4bc5f5413ef7d53cf974c112b252
Sai Harish <30901561+Saihar1sh@users.noreply.github.com>
2023-05-17 18:27:17 +0530

    added basic drawing functions, made it screen size dependent

--- CppGame/CppGame/win32_platform.cpp
+++ CppGame/CppGame/win32_platform.cpp
@@ -1,47 +1,56 @@
+#include "utils.cpp"
 #include <Windows.h>
 
-bool gameRunning = true;
+global_variable bool gameRunning = true;
 
-void* buffer_memory;
-int buffer_width;
-int buffer_height;
-BITMAPINFO buffer_bitmapInfo;
+struct Render_State
+{
+	int width, height;
+	void* memory;
+	BITMAPINFO bitmap_Info;
+};
+
+
+global_variable Render_State render_state;
+
+#include "renderer.cpp"
 
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	LRESULT result = 0;
 	switch (uMsg)
 	{
-	case WM_CLOSE:
-	case WM_DESTROY:
-	{
-		gameRunning = false;
-	}
+		case WM_CLOSE:
+		case WM_DESTROY:
+		{
+			gameRunning = false;
+		}
 			break;
 
-	case WM_SIZE:
-	{
-		RECT rect;
-		GetClientRect(hwnd, &rect);
-		buffer_width = rect.right - rect.left;
-		buffer_height = rect.bottom - rect.top;
+		case WM_SIZE:
+		{
+			RECT rect;
+			GetClientRect(hwnd, &rect);
+			render_state.width = rect.right - rect.left;
+			render_state.height = rect.bottom - rect.top;
 
-		int buffer_size = buffer_width * buffer_height * sizeof(unsigned int);
+			int buffer_size = render_state.width * render_state.height * sizeof(unsigned int);
 
-		if (buffer_memory) VirtualFree(buffer_memory, 0, MEM_RELEASE);
-		buffer_memory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+			if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
+			render_state.memory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
-		buffer_bitmapInfo.bmiHeader.biSize = sizeof(buffer_bitmapInfo.bmiHeader);
-		buffer_bitmapInfo.bmiHeader.biWidth = buffer_width;
-		buffer_bitmapInfo.bmiHeader.biHeight = buffer_height;
-		buffer_bitmapInfo.bmiHeader.biPlanes = 1;
-		buffer_bitmapInfo.bmiHeader.biBitCount = 32;
-		buffer_bitmapInfo.bmiHeader.biCompression = BI_RGB;
+			render_state.bitmap_Info.bmiHeader.biSize = sizeof(render_state.bitmap_Info.bmiHeader);
+			render_state.bitmap_Info.bmiHeader.biWidth = render_state.width;
+			render_state.bitmap_Info.bmiHeader.biHeight = render_state.height;
+			render_state.bitmap_Info.bmiHeader.biPlanes = 1;
+			render_state.bitmap_Info.bmiHeader.biBitCount = 32;
+			render_state.bitmap_Info.bmiHeader.biCompression = BI_RGB;
 
-	}
-	break;
-	default:
-		result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+		break;
+
+		default:
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
 		break;
 	}
 
@@ -76,9 +85,14 @@ int WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nSho
 		}
 
 		//Simulate
+		clear_screen(0x9ce79c);
+		//draw_rect_in_pixels(50, 50, 500, 500, 0x000000);
+		draw_rect(0, 0, 2, 2, 0x000000);
+		draw_rect(3, 2, 4, 25, 0x000000);
+		draw_rect(-3, -2, 4, 25, 0x000000);
 
 		//Rendering
-		StretchDIBits(hdc, 0, 0, buffer_width, buffer_height, 0, 0, buffer_width, buffer_height, buffer_memory, &buffer_bitmapInfo, DIB_RGB_COLORS,SRCCOPY);
+		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_Info, DIB_RGB_COLORS,SRCCOPY);
 	}
 
 };
